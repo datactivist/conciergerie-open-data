9d414c736e6f9077905dfe38791e5389ee90b500
Anthony Gigerich <anthony@datactivist.coop>
2021-02-09 17:13:43 +0100

    récup mots clés, recherche api, préparation récup feedback

--- chatbot/actions/actions.py
+++ chatbot/actions/actions.py
@@ -7,21 +7,96 @@
 
 # This is a simple example for a custom action which utters "Hello World!"
 
-# from typing import Any, Text, Dict, List
-#
-# from rasa_sdk import Action, Tracker
-# from rasa_sdk.executor import CollectingDispatcher
-#
-#
-# class ActionHelloWorld(Action):
-#
-#     def name(self) -> Text:
-#         return "action_hello_world"
-#
-#     def run(self, dispatcher: CollectingDispatcher,
-#             tracker: Tracker,
-#             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-#
-#         dispatcher.utter_message(text="Hello World!")
-#
-#         return []
+from typing import Any, Text, Dict, List
+from rasa_sdk import Action, Tracker
+from rasa_sdk.executor import CollectingDispatcher
+from rasa_sdk.events import SlotSet
+import sqlite3
+import json
+import requests
+import codecs
+import sql_query
+
+
+## Entrée: Mots clés de la recherche
+## Sortie: URL pour appeler l'api
+def get_request_keywords_url(keywords):
+
+    url = "https://trouver.datasud.fr/api/3/action/package_search?q="
+    special_characters = [
+        "!",
+        '"',
+        "#",
+        "$",
+        "%",
+        "&",
+        "'",
+        "(",
+        ")",
+        "*",
+        "+",
+        ",",
+        "-",
+        ".",
+        "/",
+        ":",
+        ";",
+        "<",
+        "=",
+        ">",
+        "?",
+        "@",
+    ]
+
+    for special_character in special_characters:
+        encoding = str(codecs.encode(special_character.encode("utf-8"), "hex"))
+        keywords = keywords.replace(
+            special_character, "%" + encoding[2 : len(encoding) - 1]
+        )
+
+    for word in keywords.split(" "):
+        url += word + "+"
+
+    return url[0 : len(url) - 1]
+
+
+# Reset the slot keywords
+class ResetSlot(Action):
+    def name(self):
+        return "action_reset_keywords_slot"
+
+    def run(self, dispatcher, tracker, domain):
+        return [SlotSet("keywords", None)]
+
+
+# Search DataSud API with keywords provided by the users and display results
+class SearchKeywordsInDatabase(Action):
+    def name(self):
+        return "action_search_database"
+
+    def run(
+        self,
+        dispatcher: CollectingDispatcher,
+        tracker: Tracker,
+        domain: Dict[Text, Any],
+    ) -> List[Dict[Text, Any]]:
+
+        request_url = get_request_keywords_url(tracker.get_slot("keywords"))
+        data = requests.post(request_url).json()
+
+        results = data["result"]["results"]
+        catalog_url = "https://trouver.datasud.fr/dataset/"
+
+        if len(results) > 0:
+            dispatcher.utter_message(text="Voici les résultats que j'ai pu trouver:\n_")
+
+            for result in results[0:5]:
+                message = ""
+                # message += result["thumbnail"] + "\n"
+                message += result["title"] + "\n"
+                message += catalog_url + result["name"] + "\n_"
+                dispatcher.utter_message(text=message)
+        else:
+            dispatcher.utter_message(
+                text="Désolé, je n'ai trouvé aucun résultat pour ta recherche."
+            )
---
+++ chatbot/actions/sql_query.py
@@ -0,0 +1,216 @@
+import sqlite3
+
+
+# Parameter: Database pointer and sql command
+# Function: Run the sql command
+def run_sql_command(cursor, sql_command, data):
+
+    try:
+        if data is not None:
+            cursor.execute(sql_command, data)
+        else:
+            cursor.execute(sql_command)
+
+        print(sql_command, data)
+
+        record = cursor.fetchall()
+
+        print(record, "\n")
+
+        return record
+
+    except sqlite3.Error as error:
+        print(
+            "\nError while running this command: \n",
+            sql_command,
+            "\n",
+            error,
+            "\n",
+        )
+
+    return None
+
+
+# Parameter: search_data = (conversation_id, keywords_user)
+# Function: Add a new search entry in the database
+def add_new_search_query(search_data):
+    database = "../rasa.db"
+
+    conversation_id = search_data[0]
+    keywords_user = search_data[1]
+
+    try:
+
+        sqliteConnection = sqlite3.connect(database)
+        cursor = sqliteConnection.cursor()
+
+        sqlite_check_feedback_exist_query = (
+            'SELECT * FROM feedback_user_search where conversation_id = "'
+            + conversation_id
+            + '" and keywords_user = "'
+            + keywords_user
+            + '";'
+        )
+
+        record = run_sql_command(cursor, sqlite_check_feedback_exist_query, None)
+
+        if len(record) == 0:
+
+            sqlite_insert_feedback_query = "INSERT INTO feedback_user_search(conversation_id, keywords_user) VALUES(?, ?);"
+            run_sql_command(
+                cursor, sqlite_insert_feedback_query, (conversation_id, keywords_user)
+            )
+
+        sqliteConnection.commit()
+        cursor.close()
+        sqliteConnection.close()
+
+    except sqlite3.Error as error:
+        print("Error while connecting to sqlite", error)
+    finally:
+        if sqliteConnection:
+            sqliteConnection.close()
+            print("Added new search query successfully\n")
+
+
+# Parameter: search_data = (conversation_id, keywords_user)
+#            feedback_augmentation_results = (keywords_augmentation, keywords_chosen)
+# Function: Add the feedback in the database
+def add_feedback_augmentation(search_data, feedback_augmentation_data):
+
+    database = "../rasa.db"
+
+    conversation_id = search_data[0]
+    keywords_user = search_data[1]
+
+    try:
+
+        sqliteConnection = sqlite3.connect(database)
+        cursor = sqliteConnection.cursor()
+
+        sqlite_check_feedback_exist_query = (
+            'SELECT search_feedback_id FROM feedback_user_search where conversation_id = "'
+            + conversation_id
+            + '" and keywords_user = "'
+            + keywords_user
+            + '";'
+        )
+
+        record = run_sql_command(cursor, sqlite_check_feedback_exist_query, None)
+
+        feedback_id = record[0][0]
+
+        if feedback_id is None:
+            # add
+            sqlite_insert_feedback_query = "INSERT INTO feedback_search_augmentation(keywords_augmentation, keywords_chosen) VALUES(?, ?);"
+            run_sql_command(
+                cursor, sqlite_insert_feedback_query, feedback_augmentation_data
+            )
+
+            sqlite_get_last_id = (
+                'SELECT id FROM feedback_search_augmentation WHERE keywords_augmentation = "'
+                + feedback_augmentation_data[0]
+                + '" and keywords_chosen = "'
+                + feedback_augmentation_data[1]
+                + '";'
+            )
+
+            record = run_sql_command(cursor, sqlite_get_last_id, None)
+
+            feedback_id = record[0][0]
+
+            sqlite_link_feedback_query = (
+                "UPDATE feedback_user_search SET search_feedback_id = "
+                + str(feedback_id)
+                + ' WHERE conversation_id = "'
+                + conversation_id
+                + '" and keywords_user = "'
+                + keywords_user
+                + '";'
+            )
+
+            run_sql_command(cursor, sqlite_link_feedback_query, None)
+        else:
+            print("Feedback already present")
+
+        sqliteConnection.commit()
+        cursor.close()
+        sqliteConnection.close()
+
+    except sqlite3.Error as error:
+        print("Error while connecting to sqlite", error)
+    finally:
+        if sqliteConnection:
+            sqliteConnection.close()
+            print("Feedback registered successfully\n")
+
+
+# Parameter: search_data = (conversation_id, keywords_user)
+#            feedback_results_data = (results, feedback_results)
+# Function: Add the feedback in the database
+def add_feedback_results(search_data, feedback_results_data):
+
+    database = "../rasa.db"
+
+    conversation_id = search_data[0]
+    keywords_user = search_data[1]
+
+    try:
+
+        sqliteConnection = sqlite3.connect(database)
+        cursor = sqliteConnection.cursor()
+
+        sqlite_check_feedback_exist_query = (
+            'SELECT results_feedback_id FROM feedback_user_search where conversation_id = "'
+            + conversation_id
+            + '" and keywords_user = "'
+            + keywords_user
+            + '";'
+        )
+
+        record = run_sql_command(cursor, sqlite_check_feedback_exist_query, None)
+
+        feedback_id = record[0][0]
+
+        if feedback_id is None:
+            sqlite_insert_feedback_query = (
+                "INSERT INTO feedback_results(results, feedback_results) VALUES(?, ?);"
+            )
+            run_sql_command(cursor, sqlite_insert_feedback_query, feedback_results_data)
+
+            sqlite_get_last_id = (
+                'SELECT id FROM feedback_results WHERE results = "'
+                + feedback_results_data[0]
+                + '" and feedback_results = "'
+                + feedback_results_data[1]
+                + '";'
+            )
+
+            record = run_sql_command(cursor, sqlite_get_last_id, None)
+
+            feedback_id = record[0][0]
+
+            sqlite_link_feedback_query = (
+                "UPDATE feedback_user_search SET results_feedback_id = "
+                + str(feedback_id)
+                + ' WHERE conversation_id = "'
+                + conversation_id
+                + '" and keywords_user = "'
+                + keywords_user
+                + '";'
+            )
+
+            run_sql_command(cursor, sqlite_link_feedback_query, None)
+        else:
+            print("Feedback Already Present")
+
+        sqliteConnection.commit()
+        cursor.close()
+        sqliteConnection.close()
+
+    except sqlite3.Error as error:
+        print("Error while connecting to sqlite", error)
+    finally:
+        if sqliteConnection:
+            sqliteConnection.close()
+            print("Feedback registered successfully\n")
