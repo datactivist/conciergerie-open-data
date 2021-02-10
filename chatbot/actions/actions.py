65705bc43fc9ed892b0c8892c95338c4ef8b1506
Anthony Gigerich <anthony@datactivist.coop>
2021-02-10 17:07:34 +0100

    recuperation du feedback via le chatbot et envoie dans la base de données

--- chatbot/actions/actions.py
+++ chatbot/actions/actions.py
@@ -10,12 +10,14 @@
 from typing import Any, Text, Dict, List
 from rasa_sdk import Action, Tracker
 from rasa_sdk.executor import CollectingDispatcher
-from rasa_sdk.events import SlotSet
+from rasa_sdk.events import SlotSet, EventType
 import sqlite3
 import json
 import requests
 import codecs
-import sql_query
+from actions import sql_query
+
+# import sql_query.py
 
 
 ## Entrée: Mots clés de la recherche
@@ -60,13 +62,135 @@ def get_request_keywords_url(keywords):
     return url[0 : len(url) - 1]
 
 
-# Reset the slot keywords
-class ResetSlot(Action):
+# Find new keywords that might interest the user
+def keywords_augmentation(keywords):
+
+    keywords_augmentation = "TO|DO|FUNCTION"
+
+    return keywords_augmentation
+
+
+# Reset the slots related to keywords
+class ResetKeywordsSlot(Action):
     def name(self):
         return "action_reset_keywords_slot"
 
     def run(self, dispatcher, tracker, domain):
-        return [SlotSet("keywords", None)]
+        return [
+            SlotSet("keywords", None),
+            SlotSet("keywords_augmentation", None),
+            SlotSet("keywords_feedback", None),
+        ]
+
+
+# Reset the slots related to results
+class ResetResultsSlot(Action):
+    def name(self):
+        return "action_reset_results_slot"
+
+    def run(self, dispatcher, tracker, domain):
+        return [
+            SlotSet("results", None),
+            SlotSet("results_feedback", None),
+        ]
+
+
+class SetKeywordsFeedback(Action):
+    def name(self):
+        return "action_set_keywords_feedback_slot"
+
+    def run(
+        self,
+        dispatcher: CollectingDispatcher,
+        tracker: Tracker,
+        domain: Dict[Text, Any],
+    ) -> List[Dict[Text, Any]]:
+
+        keywords_augmented = tracker.get_slot("keywords_augmentation")
+        keywords_feedback = tracker.get_slot("keywords_feedback")
+
+        final = ""
+        list_augmentation = keywords_augmented.split("|")
+        for feedback in keywords_feedback.split(" "):
+            feedback = int(feedback)
+            if feedback == 0:
+                return [SlotSet("keywords_feedback", "")]
+            elif feedback - 1 >= 0 and feedback - 1 < len(list_augmentation):
+                final += list_augmentation[feedback - 1] + "|"
+
+        return [SlotSet("keywords_feedback", final[0 : len(final) - 1])]
+
+
+class SetResultsFeedback(Action):
+    def name(self):
+        return "action_set_results_feedback_slot"
+
+    def run(
+        self,
+        dispatcher: CollectingDispatcher,
+        tracker: Tracker,
+        domain: Dict[Text, Any],
+    ) -> List[Dict[Text, Any]]:
+
+        results = tracker.get_slot("results")
+        results_feedback = tracker.get_slot("results_feedback")
+
+        final = ""
+        list_results = results.split("|")
+        for feedback in results_feedback.split(" "):
+            feedback = int(feedback)
+            if feedback == 0:
+                return [SlotSet("results_feedback", "")]
+            elif feedback - 1 >= 0 and feedback - 1 < len(list_results):
+                final += list_results[feedback - 1] + "|"
+
+        return [SlotSet("results_feedback", final[0 : len(final) - 1])]
+
+
+class UtterConfirmSearch(Action):
+    def name(self):
+        return "action_utter_confirm_search"
+
+    def run(
+        self,
+        dispatcher: CollectingDispatcher,
+        tracker: Tracker,
+        domain: Dict[Text, Any],
+    ) -> List[Dict[Text, Any]]:
+
+        keywords = tracker.get_slot("keywords")
+        keywords_feedback = tracker.get_slot("keywords_feedback")
+
+        message = "Tu souhaites bien faire une recherche avec ces mots-clés ?\n"
+        for keyword in keywords.split(" "):
+            message += keyword + " "
+
+        for keyword in keywords_feedback.split("|"):
+            message += keyword + " "
+
+        dispatcher.utter_message(text=message)
+
+
+class AskForKeywordsFeedbackSlotAction(Action):
+    def name(self) -> Text:
+        return "action_ask_keywords_feedback"
+
+    def run(
+        self, dispatcher: CollectingDispatcher, tracker: Tracker, domain: Dict
+    ) -> List[EventType]:
+
+        keywords_augmented = keywords_augmentation(tracker.get_slot("keywords"))
+
+        message = "Essayons d'améliorer ta recherche, peux-tu écrire les numéros des mots-clés qui te semblent pertinent ?\n\n"
+
+        message += "0 - Aucun\n"
+
+        for i, keyword in enumerate(keywords_augmented.split("|")):
+            message += str(i + 1) + " - " + keyword + "\n"
+
+        dispatcher.utter_message(text=message)
+
+        return [SlotSet("keywords_augmentation", keywords_augmented)]
 
 
 # Search DataSud API with keywords provided by the users and display results
@@ -81,22 +205,88 @@ class SearchKeywordsInDatabase(Action):
         domain: Dict[Text, Any],
     ) -> List[Dict[Text, Any]]:
 
-        request_url = get_request_keywords_url(tracker.get_slot("keywords"))
-        data = requests.post(request_url).json()
+        keywords = tracker.get_slot("keywords")
 
+        request_url = get_request_keywords_url(keywords)
+        data = requests.post(request_url).json()
         results = data["result"]["results"]
         catalog_url = "https://trouver.datasud.fr/dataset/"
 
         if len(results) > 0:
             dispatcher.utter_message(text="Voici les résultats que j'ai pu trouver:\n_")
 
-            for result in results[0:5]:
+            results_slot = ""
+            for i, result in enumerate(results[0:5]):
                 message = ""
                 # message += result["thumbnail"] + "\n"
+                message += str(i + 1) + " - "
                 message += result["title"] + "\n"
                 message += catalog_url + result["name"] + "\n_"
+                results_slot += result["title"] + "|"
                 dispatcher.utter_message(text=message)
+
+            return [SlotSet("results", results_slot)]
+
         else:
             dispatcher.utter_message(
                 text="Désolé, je n'ai trouvé aucun résultat pour ta recherche."
             )
+
+
+# Search DataSud API with keywords provided by the users and display results
+class SendSearchInfo(Action):
+    def name(self):
+        return "action_send_search_information_to_database"
+
+    def run(
+        self,
+        dispatcher: CollectingDispatcher,
+        tracker: Tracker,
+        domain: Dict[Text, Any],
+    ) -> List[Dict[Text, Any]]:
+
+        sql_query.add_new_search_query(
+            (tracker.sender_id, tracker.get_slot("keywords").replace(" ", "|"))
+        )
+
+
+# Search DataSud API with keywords provided by the users and display results
+class SendKeywordsFeedback(Action):
+    def name(self):
+        return "action_send_keywords_feedback_to_database"
+
+    def run(
+        self,
+        dispatcher: CollectingDispatcher,
+        tracker: Tracker,
+        domain: Dict[Text, Any],
+    ) -> List[Dict[Text, Any]]:
+
+        sql_query.add_feedback_augmentation(
+            (tracker.sender_id, tracker.get_slot("keywords").replace(" ", "|")),
+            (
+                tracker.get_slot("keywords_augmentation"),
+                tracker.get_slot("keywords_feedback"),
+            ),
+        )
+
+
+# Search DataSud API with keywords provided by the users and display results
+class SendResultsFeedback(Action):
+    def name(self):
+        return "action_send_results_feedback_to_database"
+
+    def run(
+        self,
+        dispatcher: CollectingDispatcher,
+        tracker: Tracker,
+        domain: Dict[Text, Any],
+    ) -> List[Dict[Text, Any]]:
+
+        sql_query.add_feedback_results(
+            (tracker.sender_id, tracker.get_slot("keywords").replace(" ", "|")),
+            (
+                tracker.get_slot("results"),
+                tracker.get_slot("results_feedback"),
+            ),
+        )
--- chatbot/actions/sql_query.py
+++ chatbot/actions/sql_query.py
@@ -27,14 +27,13 @@ def run_sql_command(cursor, sql_command, data):
             error,
             "\n",
         )
-
-    return None
+        return None
 
 
 # Parameter: search_data = (conversation_id, keywords_user)
 # Function: Add a new search entry in the database
 def add_new_search_query(search_data):
-    database = "../rasa.db"
+    database = "rasa.db"
 
     conversation_id = search_data[0]
     keywords_user = search_data[1]
@@ -63,14 +62,9 @@ def add_new_search_query(search_data):
 
         sqliteConnection.commit()
         cursor.close()
-        sqliteConnection.close()
 
     except sqlite3.Error as error:
-        print("Error while connecting to sqlite", error)
-    finally:
-        if sqliteConnection:
-            sqliteConnection.close()
-            print("Added new search query successfully\n")
+        print("-ADD_NEW_SEARCH_QUERY-\nError while connecting to sqlite", error, "\n")
 
 
 # Parameter: search_data = (conversation_id, keywords_user)
@@ -78,7 +72,7 @@ def add_new_search_query(search_data):
 # Function: Add the feedback in the database
 def add_feedback_augmentation(search_data, feedback_augmentation_data):
 
-    database = "../rasa.db"
+    database = "rasa.db"
 
     conversation_id = search_data[0]
     keywords_user = search_data[1]
@@ -88,61 +82,42 @@ def add_feedback_augmentation(search_data, feedback_augmentation_data):
         sqliteConnection = sqlite3.connect(database)
         cursor = sqliteConnection.cursor()
 
-        sqlite_check_feedback_exist_query = (
-            'SELECT search_feedback_id FROM feedback_user_search where conversation_id = "'
+        sqlite_insert_feedback_query = "INSERT INTO feedback_search_augmentation(keywords_augmentation, keywords_chosen) VALUES(?, ?);"
+        run_sql_command(
+            cursor, sqlite_insert_feedback_query, feedback_augmentation_data
+        )
+
+        sqlite_get_last_id = (
+            'SELECT id FROM feedback_search_augmentation WHERE keywords_augmentation = "'
+            + feedback_augmentation_data[0]
+            + '" and keywords_chosen = "'
+            + feedback_augmentation_data[1]
+            + '" ORDER BY id DESC;'
+        )
+
+        record = run_sql_command(cursor, sqlite_get_last_id, None)
+
+        feedback_id = record[0][0]
+
+        sqlite_link_feedback_query = (
+            "UPDATE feedback_user_search SET search_feedback_id = "
+            + str(feedback_id)
+            + ' WHERE conversation_id = "'
             + conversation_id
             + '" and keywords_user = "'
             + keywords_user
             + '";'
         )
 
-        record = run_sql_command(cursor, sqlite_check_feedback_exist_query, None)
-
-        feedback_id = record[0][0]
-
-        if feedback_id is None:
-            # add
-            sqlite_insert_feedback_query = "INSERT INTO feedback_search_augmentation(keywords_augmentation, keywords_chosen) VALUES(?, ?);"
-            run_sql_command(
-                cursor, sqlite_insert_feedback_query, feedback_augmentation_data
-            )
-
-            sqlite_get_last_id = (
-                'SELECT id FROM feedback_search_augmentation WHERE keywords_augmentation = "'
-                + feedback_augmentation_data[0]
-                + '" and keywords_chosen = "'
-                + feedback_augmentation_data[1]
-                + '";'
-            )
-
-            record = run_sql_command(cursor, sqlite_get_last_id, None)
-
-            feedback_id = record[0][0]
-
-            sqlite_link_feedback_query = (
-                "UPDATE feedback_user_search SET search_feedback_id = "
-                + str(feedback_id)
-                + ' WHERE conversation_id = "'
-                + conversation_id
-                + '" and keywords_user = "'
-                + keywords_user
-                + '";'
-            )
-
-            run_sql_command(cursor, sqlite_link_feedback_query, None)
-        else:
-            print("Feedback already present")
+        run_sql_command(cursor, sqlite_link_feedback_query, None)
 
         sqliteConnection.commit()
         cursor.close()
-        sqliteConnection.close()
 
     except sqlite3.Error as error:
-        print("Error while connecting to sqlite", error)
-    finally:
-        if sqliteConnection:
-            sqliteConnection.close()
-            print("Feedback registered successfully\n")
+        print(
+            "-ADD_FEEDBACK_AUGMENTATION-\nError while connecting to sqlite", error, "\n"
+        )
 
 
 # Parameter: search_data = (conversation_id, keywords_user)
@@ -150,7 +125,7 @@ def add_feedback_augmentation(search_data, feedback_augmentation_data):
 # Function: Add the feedback in the database
 def add_feedback_results(search_data, feedback_results_data):
 
-    database = "../rasa.db"
+    database = "rasa.db"
 
     conversation_id = search_data[0]
     keywords_user = search_data[1]
@@ -160,57 +135,46 @@ def add_feedback_results(search_data, feedback_results_data):
         sqliteConnection = sqlite3.connect(database)
         cursor = sqliteConnection.cursor()
 
-        sqlite_check_feedback_exist_query = (
-            'SELECT results_feedback_id FROM feedback_user_search where conversation_id = "'
+        sqlite_insert_feedback_query = (
+            "INSERT INTO feedback_results(results, feedback_results) VALUES(?, ?);"
+        )
+        run_sql_command(cursor, sqlite_insert_feedback_query, feedback_results_data)
+
+        sqlite_get_last_id = (
+            'SELECT id FROM feedback_results WHERE results = "'
+            + feedback_results_data[0]
+            + '" and feedback_results = "'
+            + feedback_results_data[1]
+            + '" ORDER BY id DESC;'
+        )
+
+        record = run_sql_command(cursor, sqlite_get_last_id, None)
+
+        feedback_id = record[0][0]
+
+        sqlite_link_feedback_query = (
+            "UPDATE feedback_user_search SET results_feedback_id = "
+            + str(feedback_id)
+            + ' WHERE conversation_id = "'
             + conversation_id
             + '" and keywords_user = "'
             + keywords_user
             + '";'
         )
 
-        record = run_sql_command(cursor, sqlite_check_feedback_exist_query, None)
-
-        feedback_id = record[0][0]
-
-        if feedback_id is None:
-            sqlite_insert_feedback_query = (
-                "INSERT INTO feedback_results(results, feedback_results) VALUES(?, ?);"
-            )
-            run_sql_command(cursor, sqlite_insert_feedback_query, feedback_results_data)
-
-            sqlite_get_last_id = (
-                'SELECT id FROM feedback_results WHERE results = "'
-                + feedback_results_data[0]
-                + '" and feedback_results = "'
-                + feedback_results_data[1]
-                + '";'
-            )
-
-            record = run_sql_command(cursor, sqlite_get_last_id, None)
-
-            feedback_id = record[0][0]
-
-            sqlite_link_feedback_query = (
-                "UPDATE feedback_user_search SET results_feedback_id = "
-                + str(feedback_id)
-                + ' WHERE conversation_id = "'
-                + conversation_id
-                + '" and keywords_user = "'
-                + keywords_user
-                + '";'
-            )
-
-            run_sql_command(cursor, sqlite_link_feedback_query, None)
-        else:
-            print("Feedback Already Present")
+        run_sql_command(cursor, sqlite_link_feedback_query, None)
 
         sqliteConnection.commit()
         cursor.close()
-        sqliteConnection.close()
 
     except sqlite3.Error as error:
-        print("Error while connecting to sqlite", error)
-    finally:
-        if sqliteConnection:
-            sqliteConnection.close()
-            print("Feedback registered successfully\n")
+        print("-ADD_FEEDBACK_RESULTS-\nError while connecting to sqlite", error, "\n")
+
+
+# sqliteconnect = sqlite3.connect("../rasa.db")
+# cursor = sqliteconnect.cursor()
+# run_sql_command(
+#    cursor,
+#    'SELECT results_feedback_id FROM feedback_user_search where conversation_id = "ad48ad0849f04ca7a26aa1fdb2d273e0" and keywords_user = "barrages"; ',
+#    None,
+# )
